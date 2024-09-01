c69387e68f76b4cd416cdb31be757e8b486737d3
sohamkamani <sohamkamani@gmail.com>
2024-09-01 17:48:11 +0530

    working example

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# {TITLE}
+# Using the Vertex (Google Cloud) Gemini API in Java
 
-This is the example repo for my blog post on [{TITLE}]({URL})
+This is the example repo for my blog post on [Using the Vertex (Google Cloud) Gemini API in Java](https://sohamkamani.com/java/vertex-gemini-api)
 
 To run this example:
 
 1. Make sure you have Java, JDK, and Maven installed
 2. Clone the repo
 3. Run `mvn clean compile assembly:single` - this should give you a JAR file in the `target` folder
-4. Run `java -jar target/{PROJECTDIR}-1.0-SNAPSHOT-jar-with-dependencies.jar` to run the JAR file
+4. Run `java -jar target/vertex-gemini-java-1.0-SNAPSHOT-jar-with-dependencies.jar` to run the JAR file
--- src/main/java/com/sohamkamani/App.java
+++ src/main/java/com/sohamkamani/App.java
@@ -1,20 +1,65 @@
 package com.sohamkamani;
 
-import com.github.ricksbrown.cowsay.Cowsay;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import com.google.cloud.vertexai.VertexAI;
+import com.google.cloud.vertexai.api.GenerateContentResponse;
+import com.google.cloud.vertexai.api.GenerationConfig;
+import com.google.cloud.vertexai.api.HarmCategory;
+import com.google.cloud.vertexai.api.SafetySetting;
+import com.google.cloud.vertexai.generativeai.ContentMaker;
+import com.google.cloud.vertexai.generativeai.GenerativeModel;
 
 /**
- * Hello world!
- *
+ * This is a simple example of how to use the Vertex AI Generative AI API to generate content.
  */
 public class App {
-    public static void main(String[] args) {
-        int result = Calculator.add(4, 5);
-        String[] cowArgs = new String[] { String.valueOf(result) };
-        String cowString = Cowsay.say(cowArgs);
-        System.out.printf(cowString);
-    }
+	public static void main(String[] args) throws IOException {
+		try (VertexAI vertexAi = new VertexAI("sohamkamani-demo", "us-central1");) {
+			GenerationConfig generationConfig =
+					GenerationConfig.newBuilder()
+							.setMaxOutputTokens(8192)
+							.setTemperature(1F)
+							.setTopP(0.95F)
+							.build();
+			List<SafetySetting> safetySettings = Arrays.asList(
+					SafetySetting.newBuilder()
+							.setCategory(HarmCategory.HARM_CATEGORY_HATE_SPEECH)
+							.setThreshold(SafetySetting.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE)
+							.build(),
+					SafetySetting.newBuilder()
+							.setCategory(HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT)
+							.setThreshold(SafetySetting.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE)
+							.build(),
+					SafetySetting.newBuilder()
+							.setCategory(HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT)
+							.setThreshold(SafetySetting.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE)
+							.build(),
+					SafetySetting.newBuilder()
+							.setCategory(HarmCategory.HARM_CATEGORY_HARASSMENT)
+							.setThreshold(SafetySetting.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE)
+							.build());
+			GenerativeModel model =
+					new GenerativeModel.Builder()
+							.setModelName("gemini-1.5-flash-001")
+							.setVertexAi(vertexAi)
+							.setGenerationConfig(generationConfig)
+							.setSafetySettings(safetySettings)
+							.build();
 
-    public static int add(int n1, int n2) {
-        return n1 + n2;
-    }
+			var text1 =
+					"Write a short story about a robot who learns to love the smell of freshly baked bread. Make sure it's funny and heartwarming.";
+
+			var content = ContentMaker.fromMultiModalData(text1);
+			GenerateContentResponse response =
+					model.generateContent(content);
+
+			System.out.println("Response:" + response);
+			// Do something with the response
+			// responseStream.stream().forEach(System.out::println);
+		}
+
+
+	}
 }
